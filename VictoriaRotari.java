8d07c3f82367e3603a42119875295cad85ca5068
Victoria Rotari <rotarivictoria@yahoo.com>
2017-10-08 13:24:46 -0700

    VictoriaRotari.java

---
+++ VictoriaRotari.java
@@ -0,0 +1,18 @@
+
+public class VictoriaRotari {
+	
+ public static void main (String [] args) {
+ 
+ System.out.println("My name is Victoria.");
+ System.out.println("I worked for the E-Commerce store for 2 years as Online Sales Operator.");
+ System.out.println("For the las 11 months I'm with Amazon Marketing Voice Shopping team.");
+ System.out.println("Our team's goal is to encourage Alexa users to form daily shopping habbits by creating efortless shopping environment." );
+ System.out.println("I work cros-functionally with reatil teams and tesing team to ensure the smooth deals launches.");
+ System.out.println("I manage the deals submission and promotion approval process on a daily basis.");
+ System.out.println("I coordinate our master tracker that includes data entry and data validation." );
+ System.out.println("I'm responsible for the submission and testing golden utterances, shopping commands that guarantee to surface a specific product when spoken to Alexa device.");
+ System.out.println("I perform setup and configuration of Alexa exclusive promos.");
+ System.out.println("I monitor and comunicate program results and metrics to external and internal team regularly.");
+ 
+}
+}
